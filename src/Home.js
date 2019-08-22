41e14595228e94922db55b1be0115998796214b8
github username <github email address>
2019-08-22 08:15:30 -0400

    done

--- src/Home.js
+++ src/Home.js
@@ -13,13 +13,21 @@ class Home extends Component {
 
 
       <div >
-      
 
 
-      <div className="pictures" id="cf">
-      <img className="bottom" src="https://cdn.vox-cdn.com/thumbor/n6dzbQc_SjIpJKETDxctIajWRkk=/0x0:3000x2002/1200x800/filters:focal(1260x761:1740x1241)/cdn.vox-cdn.com/uploads/chorus_image/image/64022593/GettyImages_171171258.0.jpg" />
-      <img className="top" src="https://images.squarespace-cdn.com/content/v1/5af0e0bc9772aea4d08a4628/1529105627870-T2MQK9VQNP1FIOBJBD6R/ke17ZwdGBToddI8pDm48kOggE0Ch6pMGalwtLMqzsSB7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Ufo5RWkg_J4of0jUNHaDHx6pZKBvpVYzidBWCapg0tuoMuEaB2HPGSYDV-11UTcW2g/Afropunk_101516-42.jpg" />
-      <img className="bottom" src="https://assets3.thrillist.com/v1/image/1628198/size/tmg-article_default_mobile;jpeg_quality=20.jpg"/>
+
+      <div  id="cf">
+      <img src="/IMG_1908 (1).JPG" />
+      <img src="/IMG_2102.JPEG" />
+      <img src="/IMG_2196.JPEG"/>
+      <img src="/IMG_2925.JPEG"/>
+      <img src="/IMG_2196.JPEG"/>
+      <img src="/IMG_3192.JPEG"/>
+      <img src="/7F0894FE-D8AC-4249-9B0C-BA74718913AD.jpg"/>
+      <img src="/AD7C3EA1-CB6C-4367-AC97-DC4B9F329463.jpg"/>
+      <img src="/57023878355__09673FE5-E163-4DFB-BDD1-EB3C200D5292.JPG"/>
+      <img src="/dog.jpeg"/>
+      <img src="/pretty.jpeg"/>
     </div>
 
 
